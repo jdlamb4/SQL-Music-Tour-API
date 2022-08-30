ad622043e2c300903a62e96d4dbe47d08736bd4c
Jacob Lamb <jacoblamb15@gmail.com>
2022-08-30 05:41:08 -0400

    finished activity 8.4.4

--- models/band.js
+++ models/band.js
@@ -1,6 +1,6 @@
 // DEPENDENCIES
 const { Sequelize, DataTypes,  Model } = require('sequelize')
-const sequelize = new Sequelize(process.env.PG_URI)
+// const sequelize = new Sequelize(process.env.PG_URI) no longer need with the sequelize-cli
 
 // MODEL
 class Band extends Model{}
@@ -30,7 +30,7 @@ Band.init({
 }, {
     sequelize,                           
     modelName: 'Band',
-    tableName: 'band',
+    tableName: 'bands',
     timestamps: false
 }) 
 
--- server.js
+++ server.js
@@ -19,12 +19,12 @@ const sequelize = new Sequelize({
 // SEQUELIZE CONNECTION
 // const sequelize = new Sequelize(process.env.PG_URI)
 
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
+// try {
+//     sequelize.authenticate() 
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+// } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`) 
+// }
 
 
 // ROOT
